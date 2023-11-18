5c939b4fa7d5364149c2203664be3dee67bc2e87
João Guarany <joao.guarany@parceiro.gruposaltaedu.com>
2023-11-18 20:55:26 -0300

    chore: eslint install

--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
-import { StatusBar } from 'react-native';
-import { ThemeProvider } from 'styled-components'
-import { NavigationContainer } from '@react-navigation/native';
-import Routes from './src/routes';
+import React from "react";
+import { StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components";
+import { NavigationContainer } from "@react-navigation/native";
+import Routes from "./src/routes";
 
-import { GlobalStyles } from '../my-app/src/styles/global-styles'
-import { theme } from './src/styles/theme';
+import { GlobalStyles } from "./src/styles/global-styles";
+import { theme } from "./src/styles/theme";
 
 export default function App() {
   return (
-      <ThemeProvider theme={theme}>
-        <NavigationContainer>
-          <StatusBar backgroundColor="#0671E0" barStyle={'light-content'} />
-          <Routes />
-          <GlobalStyles />
-        </NavigationContainer>
-      </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <NavigationContainer>
+        <StatusBar backgroundColor="#0671E0" barStyle={"light-content"} />
+        <Routes />
+        <GlobalStyles />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
