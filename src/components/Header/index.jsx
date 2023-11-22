77596fc5ab44475cd4e1af96f20655d0ad7b1899
mwises <88902469+mwises@users.noreply.github.com>
2023-11-22 16:08:50 -0300

    Componente Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,14 +1,41 @@
-import React from 'react';
-import * as Styled from './styles';
-import { View, Text } from 'react-native';
+import React from "react";
+import Heading from "../Heading";
+import { FaChevronLeft } from "react-icons/fa";
 
-
-export default function HeaderComponent({ title }) {
-    return (
-
-        <Styled.Header>
-            <Text style={{ color: '#fff' }}>{title}</Text>
-        </Styled.Header>
-
-    );
-};
+export default function HeaderComponent({ title, content = true }) {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        width: "100%",
+        height: "65px",
+        background: "#0671E0",
+      }}
+    >
+      <div
+        style={{
+          maxWidth: "398px",
+          margin: "13.5px auto",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: content ? "space-between" : "flex-start",
+        }}
+      >
+        <FaChevronLeft
+          style={{
+            display: content ? "block" : "none",
+            fontSize: "24px",
+            height: "28px",
+            color: "#fff",
+          }}
+        />
+        <Heading children={title} />
+        <div
+          style={{ 
+            display: content ? "block" : "none", 
+            width: "24px" }}
+        ></div>
+      </div>
+    </div>
+  );
+}
