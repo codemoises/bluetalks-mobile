ee9cc123ffb0f623d9892c5754bcb5ef12871e6c
mwises <88902469+mwises@users.noreply.github.com>
2023-11-22 16:09:55 -0300

    Página das figuras favoritas

--- src/pages/Favorites/index.jsx
+++ src/pages/Favorites/index.jsx
@@ -1,10 +1,10 @@
-import { Text, View } from 'react-native';
-import HeaderComponent from '../../components/Header';
+import { Text, View } from "react-native";
+import HeaderComponent from "../../components/Header";
 
 export default function Favorites() {
   return (
-        <View>
-            <HeaderComponent title={'Favoritos'} />
-        </View>
+    <View>
+      <HeaderComponent title={"Favoritos"} />
+    </View>
   );
-};
+}
