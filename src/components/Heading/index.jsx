5054ea18efb839ed37adb333501bdbd5d50fc3c8
mwises <88902469+mwises@users.noreply.github.com>
2023-11-24 15:45:02 -0300

    Componente Heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -1,15 +1,16 @@
-export default function Heading({ children, fontSize = 28, colorDark = true}) {
+import { Text } from "react-native";
+
+export default function Heading({ children, colorDark = true}) {
   return (
-    <h1
+    <Text
       style={{
         padding: "0 16px",
         margin: 0,
         color: colorDark ? "#212121" : "#fff",
-        fontSize: fontSize,
         fontWeight: "bold",
       }}
     >
       {children}
-    </h1>
+    </Text>
   );
 }
