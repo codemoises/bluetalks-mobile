550c461b13b1be67c47325b6c94f4b349a5ac8cb
mwises <88902469+mwises@users.noreply.github.com>
2023-11-24 15:44:20 -0300

    Componente Figure

--- src/components/Figure/index.jsx
+++ src/components/Figure/index.jsx
@@ -1,8 +1,9 @@
+import { View, Image } from "react-native";
 import Heading from "../Heading";
 
-export default function Figure({ FigureSrc, FigureAlt, title, figureAudio }) {
+export default function Figure({ FigureSrc, title }) {
   return (
-    <div
+    <View
       style={{
         width: "125px",
         height: "164px",
@@ -11,12 +12,22 @@ export default function Figure({ FigureSrc, FigureAlt, title, figureAudio }) {
         alignItems: "center",
       }}
     >
-      <div style={{ width: "100%", height: "140px", borderRadius: '12px', border: '1px solid #212121' }}>
-        <img style={{ borderRadius: '12px', width: "100%", height: "100%" }} src={FigureSrc} alt={FigureAlt} />
-      </div>
-      <div style={{ margin: "4px 0" }}>
-        <Heading children={title} colorDark={true} fontSize={"14px"} />
-      </div>
-    </div>
+      <View
+        style={{
+          width: "100%",
+          height: "140px",
+          borderRadius: "12px",
+          border: "1px solid #212121",
+        }}
+      >
+        <Image
+          style={{ borderRadius: 12, width: "100%", height: "100%" }}
+          source={{ uri: FigureSrc }}
+        />
+      </View>
+      <View style={{ margin: "4px 0" }}>
+        <Heading children={title} colorDark={true} />
+      </View>
+    </View>
   );
 }
