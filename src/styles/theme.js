cb63b407c0559e087374a1153083d5061a242aa9
mwises <88902469+mwises@users.noreply.github.com>
2023-11-26 18:00:24 -0300

    Estilização global

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,18 +1,29 @@
 export const theme = {
-  colors: {
-    D_Blue: '#0053AD',
-    Navy: '#0663C7',
-    mblue: '#0671E0',
-    Azure: '4196F0',
-    Sky: '#DBEDFF',
-    L_Blue: '#EEF5FC',
-    white: '#FFFFFF',
-  },
-  font: {
-    family: {
-      default: "'Inter', sans-serif",
+    colors: {
+      D_Blue: '#0053AD',
+      Navy: '#0663C7',
+      mblue: '#0671E0',
+      Azure: '4196F0',
+      Sky: '#DBEDFF',
+      L_Blue: '#EEF5FC',
+      white: '#FFFFFF',
     },
-    sizes: {
+    font: {
+      family: {
+        default: "'Inter', sans-serif",
+      },
+      sizes: {
+        xsmall: '8rem',
+        small: '1.6rem',
+        medium: '2.4rem',
+        large: '3.2rem',
+        xlarge: '4.0rem',
+        xxlarge: '4.8rem',
+        huge: '5.6rem',
+        xhuge: '6.4rem',
+      },
+    },
+    spacings: {
       xsmall: '8rem',
       small: '1.6rem',
       medium: '2.4rem',
@@ -22,15 +33,5 @@ export const theme = {
       huge: '5.6rem',
       xhuge: '6.4rem',
     },
-  },
-  spacings: {
-    xsmall: '8rem',
-    small: '1.6rem',
-    medium: '2.4rem',
-    large: '3.2rem',
-    xlarge: '4.0rem',
-    xxlarge: '4.8rem',
-    huge: '5.6rem',
-    xhuge: '6.4rem',
-  },
-};
+  };
+  
