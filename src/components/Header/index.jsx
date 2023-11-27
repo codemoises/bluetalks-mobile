6fdb7d8b3e76abf27585f7cef0fb31f56639a38d
mwises <88902469+mwises@users.noreply.github.com>
2023-11-27 15:01:53 -0300

    Componente Header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,44 @@
+import Heading from "../Heading";
+import { Feather } from '@expo/vector-icons';
+import { View } from "react-native";
+
+export default function HeaderComponent({ title, content }) {
+  return (
+    <View
+      style={{
+        position: 'fixed',
+        width: "100%",
+        height: 95,
+        backgroundColor: "#0671E0",
+      }}
+    >
+      <View
+        style={{
+          marginLeft: 25,
+          marginRight: 40,
+          paddingTop: 20,
+          height: "100%",
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: content ? "space-between" : "flex-start",
+        }}
+      >
+        <Feather
+          name="chevron-left"
+          size={24}
+          style={{
+            display: content ? "block" : "none",
+            color: "#fff",
+          }}
+        />
+        <Heading children={title} colorDark={false} size={28} />
+        <View
+          style={{ 
+            display: content ? "block" : "none", 
+            width: "24px" }}
+        ></View>
+      </View>
+    </View>
+  );
+};
