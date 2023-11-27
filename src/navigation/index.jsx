cc255a19b2485e28af1bfd1ca06a82988f8964b2
mwises <88902469+mwises@users.noreply.github.com>
2023-11-27 15:02:29 -0300

    Navigation

--- src/navigation/index.jsx
+++ src/navigation/index.jsx
@@ -1,6 +1,11 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import Favorites from "../screens/Favorites";
+import Search from "../screens/Search";
+import Notification from "../screens/Notification";
+import Profile from "../screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -8,7 +13,7 @@ export default function Navigation() {
   return (
     <Tab.Navigator
       initialRouteName="Home"
-      tabBArOptions={{ showLabel: false }}
+      tabBarOptions={{ showLabel: false }}
       screenOptions={{
         tabBarStyle: {
           position: "absolute",
@@ -20,15 +25,99 @@ export default function Navigation() {
       }}
     >
       <Tab.Screen
+        name="Favorites"
+        component={Favorites}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            if (focused)
+              return (
+                <Feather name="star" size={25} style={{ color: "white" }} />
+              );
+            else
+              return (
+                <Feather name="star" size={25} style={{ color: "black" }} />
+              );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Pesquisar"
+        component={Search}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            if (focused)
+              return (
+                <Ionicons name="ios-search" size={25} style={{ color: "white" }} />
+              );
+            else
+              return (
+                <Ionicons name="ios-search" size={25} style={{ color: "black" }} />
+              );
+          },
+        }}
+      />
+      <Tab.Screen
         name="Home"
         component={Home}
         options={{
           headerShown: false,
-          tabBarIcon: ({focused, size, color }) => {
+          tabBarIcon: ({ focused }) => {
             if (focused)
-              return <Ionicons name="home" size={size} color={color} />
+              return (
+                <Ionicons name="home" size={25} style={{ color: "white" }} />
+              );
             else
-              return <Ionicons name="home-outline" size={size} color={color} />
+              return (
+                <Ionicons
+                  name="home-outline"
+                  size={25}
+                  style={{ color: "black" }}
+                />
+              );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Notificações"
+        component={Notification}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            if (focused)
+              return (
+                <Feather name="bell" size={25} style={{ color: "white" }} />
+              );
+            else
+              return (
+                <Feather
+                  name="bell"
+                  size={25}
+                  style={{ color: "black" }}
+                />
+              );
+          },
+        }}
+      />
+      <Tab.Screen
+        name="Perfil"
+        component={Profile}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ focused }) => {
+            if (focused)
+              return (
+                <Feather name="user" size={25} style={{ color: "white" }} />
+              );
+            else
+              return (
+                <Feather
+                  name="user"
+                  size={25}
+                  style={{ color: "black" }}
+                />
+              );
           },
         }}
       />
