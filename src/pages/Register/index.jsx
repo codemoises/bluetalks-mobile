6a64af322872a8ccbfeb956d75b6193958fd7e14
mwises <88902469+mwises@users.noreply.github.com>
2023-11-28 16:21:47 -0300

    Pages Register e Login

---
+++ src/pages/Register/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import HeaderComponent from '../../components/Header';
+
+export default function Register() {
+    return (
+        <View>
+            <HeaderComponent title='LOGIN' />
+        </View>
+    );
+};
