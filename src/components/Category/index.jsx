4c3b2bba2c37d12d9fac6b6ed7e540af5ab6e65b
mwises <88902469+mwises@users.noreply.github.com>
2023-11-28 13:05:51 -0300

    Componentes Category e Figure

---
+++ src/components/Category/index.jsx
@@ -0,0 +1,33 @@
+import { View, Image } from "react-native";
+import Heading from "../Heading";
+
+export default function Category({ categorySrc, title }) {
+  return (
+    <View
+      style={{
+        width: 125,
+        height: 164,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
+    >
+      <View
+        style={{
+          width: "100%",
+          height: 140,
+          borderRadius: 12,
+          border: "1px solid #212121",
+        }}
+      >
+        <Image
+          style={{ borderRadius: 12, width: "100%", height: "100%" }}
+          source={{ uri: categorySrc }}
+        />
+      </View>
+      <View style={{ margin: "4px 0" }}>
+        <Heading children={title} colorDark={true} size={16} />
+      </View>
+    </View>
+  );
+}
