295a82fe09e3ad4deb7d07cc63998b99d7a44477
mwises <88902469+mwises@users.noreply.github.com>
2023-11-30 19:52:22 -0300

    Update index.jsx

--- src/components/TextInput/index.jsx
+++ src/components/TextInput/index.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
 import Heading from "../../components/Heading";
 import { View, TextInput } from "react-native";
 
-export default function TextInputComponent({ title, placeholder, type, secure }) {
-
-    return (
-        <View>
-            <Heading children={title} colorDark={true} size={16} />
-            <TextInput style={{ borderBottomWidth: 1, marginBottom: 24, width: 300, height: 40 }} placeholder={placeholder} textContentType={type} secureTextEntry={secure} />
-        </View>
-    );
+export default function TextInputComponent({ title, placeholder, secure }) {
+  return (
+    <View>
+      <Heading children={title} colorDark={true} size={16} />
+      <TextInput
+        style={{
+          borderBottomWidth: 1,
+          marginBottom: 24,
+          width: 300,
+          height: 40,
+        }}
+        placeholder={placeholder}
+        secureTextEntry={secure}
+      />
+    </View>
+  );
 }
