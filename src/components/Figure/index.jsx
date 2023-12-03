a979d5a74a8197e420cf1ec263616f814638c5c9
mwises <88902469+mwises@users.noreply.github.com>
2023-12-03 15:18:57 -0300

    Componente Figure

--- src/components/Figure/index.jsx
+++ src/components/Figure/index.jsx
@@ -1,14 +1,22 @@
-import { View, Image, TouchableNativeFeedback } from "react-native";
+import {
+  View,
+  Image,
+  TouchableNativeFeedback,
+  TouchableOpacity
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
 import Heading from "../Heading";
-import { Audio } from 'expo-av';
+import { Audio } from "expo-av";
 import React from "react";
- 
-export default function Figure({ imageSrc, audioSrc, title }) {
+
+export default function Figure({ imageSrc, audioSrc, title, favorite }) {
   const [sound, setSound] = React.useState();
+  const [favorited, setFavorited] = React.useState(favorite);
 
   async function playSound() {
     const { sound } = await Audio.Sound.createAsync({
-      uri: audioSrc
+      uri: audioSrc,
     });
     setSound(sound);
 
@@ -25,15 +33,18 @@ export default function Figure({ imageSrc, audioSrc, title }) {
 
   return (
     <TouchableNativeFeedback onPress={playSound}>
-      <View style={{
-        width: 125,
-        height: 164,
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-      }}>
+      <View
+        style={{
+          width: 125,
+          height: 164,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <View
           style={{
+            position: "relative",
             width: "100%",
             height: 140,
             borderRadius: 12,
@@ -44,6 +55,20 @@ export default function Figure({ imageSrc, audioSrc, title }) {
             style={{ borderRadius: 12, width: "100%", height: "100%" }}
             source={{ uri: imageSrc }}
           />
+        </View>
+        <View style={{ position: "absolute", top: 6, right: 6 }}>
+          <TouchableOpacity>
+            {favorited ? (
+              <AntDesign name="staro" size={21} color="#4D4D4D" />
+            ) : (
+              <AntDesign name="star" size={24} color="#FFD700" />
+            )}
+          </TouchableOpacity>
+        </View>
+        <View style={{ position: "absolute", bottom: 32, right: 6 }}>
+          <TouchableOpacity>
+            <Feather name="edit" size={21} color="#4D4D4D" />
+          </TouchableOpacity>
         </View>
         <View style={{ margin: "4px 0" }}>
           <Heading children={title} colorDark={true} size={16} />
