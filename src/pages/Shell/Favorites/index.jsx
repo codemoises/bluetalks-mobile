dfc8ad7d765cc90e91d605508a2f9eecf3381941
mwises <88902469+mwises@users.noreply.github.com>
2023-12-03 17:16:54 -0300

    Shell Favorites e Profile

--- src/pages/Shell/Favorites/index.jsx
+++ src/pages/Shell/Favorites/index.jsx
@@ -1,15 +1,10 @@
 import { View } from "react-native";
-import Heading from "../../../components/Heading";
 import HeaderComponent from "../../../components/Header";
-import { Link } from '@react-navigation/native';
-
 export default function Favorites() {
   return (
     <View>
-      <HeaderComponent title={"Favoritos"} content={true}  />
-      <View style={{ margin: 85 }}>
-        <Heading children={"fodaseeee"} colorDark={true} />
-      </View>
+      <HeaderComponent title={"Favoritos"} content={true} />
     </View>
   );
 }
+
--- src/pages/Shell/Profile/index.jsx
+++ src/pages/Shell/Profile/index.jsx
@@ -12,7 +12,7 @@ export default function Favorites() {
 
   return (
     <View style={{ justifyContent: "center", alignItems: "center" }}>
-      <HeaderComponent content={true} />
+      <HeaderComponent />
       <ProfileInformation
         profileSrc={
           "https://images.unsplash.com/photo-1516028437832-1394f2532d73?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
